f4a1262912a5f9d69f45185cb56bcba88cea7b13
Leonid Korokh <lkorokh@gmail.com>
2019-05-14 15:03:51 +0300

    change user nameadd DOCKER_ACCOUNT variable

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 declare -a VERSIONS=(32RC2-php56 32RC2-php70 32RC2-php71 32RC2-php72 32RC2-php73)
+declare DOCKER_ACCOUNT=red55
 
 for TAOVER in ${VERSIONS}
 do
     echo "Building image of tao, version is $TAOVER"
-    docker build -f Dockerfile-$TAOVER -t alroniks/tao:$TAOVER .
-    docker push alroniks/tao:$TAOVER
+    docker build -f Dockerfile-$TAOVER -t $DOCKER_ACCOUNT/tao:$TAOVER .
+    docker push $DOCKER_ACCOUNT/tao:$TAOVER
 done
 
